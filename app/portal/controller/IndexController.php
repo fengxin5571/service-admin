8e94cd71b3e05875c61199dbb03ee9ff34f810ff
fengxin <502251245@qq.com>
2019-01-03 17:29:48 +0800

    2019 1 3 新增数字套件

--- app/portal/controller/IndexController.php
+++ app/portal/controller/IndexController.php
@@ -16,8 +16,17 @@ use tree\Tree;
 
 class IndexController extends HomeBaseController
 {
+    /*
+     * 官网首页
+     */
     public function index()
     {
         return $this->fetch(':netWork');
+    }
+    /*
+     * 数字套件
+     */
+    public function numberSuite(){
+        return $this->fetch(':numberSuite');
     }
 }
